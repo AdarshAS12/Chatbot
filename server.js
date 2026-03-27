eb4f964d5aaa9025d088f90910e14cbe5bd5cd8a
Adarsh A S <99171476+AdarshAS12@users.noreply.github.com>
2026-03-27 16:43:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -509,7 +509,7 @@ async function askWeight(user) {
 /////////////////////////////////////////////////////
 // START
 /////////////////////////////////////////////////////
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Running on ${PORT}`);
 });
